dc0561f6a9c07124c1ec58845c75862c331dcd9c
Blake Rose <bdecktor@gmail.com>
2019-07-05 23:50:35 +0000

    first commit on v5

---
+++ gjua_v5/lib/environment.rb
@@ -0,0 +1,8 @@
+require_relative "./gjua_v5/version"
+
+module GjuaV5
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+require_relative './cli'
---
+++ gjua_v5/lib/gjua_v5/version.rb
@@ -0,0 +1,3 @@
+module GjuaV5
+  VERSION = "0.1.0"
+end
