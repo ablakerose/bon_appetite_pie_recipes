35e4454b70f884ac9b74fd7660536962147aa0a9
Blake Rose <bdecktor@gmail.com>
2019-07-15 03:08:58 +0000

    add require colorize dependancy

--- bon_app_pies/lib/bon_app_pies/scraper.rb
+++ bon_app_pies/lib/bon_app_pies/scraper.rb
@@ -24,7 +24,6 @@ class Scraper
     pie.directions = doc.css(".steps li").text
     #both pie.ingredients and pie.directions are arrays. I want to know how i can iterate over them to ensure that each new element of the array begins on a new line
     
-    
   end
   
   # def self.ingredients_as_list
